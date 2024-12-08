5f420cd682cfe39c6e2a54c12491947af0e93107
nemanjabenkovic <evolroop@naver.com>
2024-12-09 04:38:56 +0900

    review cell

---
+++ TakeMyDump/TakeMyDump/ViewController/ReviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReviewViewController.swift
+//  TakeMyDump
+//
+//  Created by 신민규 on 12/8/24.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
